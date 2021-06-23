89fcf87323985ccb072d2ad89a5243c0f509eee1
HiTechBeing <hitechbeing@gmail.com>
2021-06-23 08:08:07 +0100

    Added endpoint for crating users and authenticating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8qdiz1rq^nx2+y%-%tb934-ri-j81ysci#)4cwy3b!%b&#yek=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
